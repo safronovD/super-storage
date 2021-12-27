7cf8e7ed7694ec142b0c7da1d51c438f15eb490a
safronovD <danil_safronov@dell.com>
2021-12-27 13:40:59 +0300

    add cli
    Signed-off-by: safronovD <danil_safronov@dell.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,44 +1,103 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
+	"flag"
 	"github.com/safronovD/super-storage/pkg"
+	"log"
 )
 
-func main() {
-	fmt.Println("Implement me pls")
+const (
+	fsPath = "/tmp/data"
+)
 
+// flags
+var (
+	modeR = flag.Bool("read", false, "read")
+	modeW = flag.Bool("write", false, "write")
+	modeD = flag.Bool("delete", false, "delete")
+	modeL = flag.Bool("list", false, "list")
+
+	path = flag.String("path", "", "path")
+	name = flag.String("name", "", "name")
+	size = flag.Uint("size", 256, "size")
+	hash = flag.String("hash", "SHA256", "hash")
+)
+
+func performWrite(name, path, hash string, blockSize uint) error {
 	manager := &pkg.FSManager{}
 
-	writer, err := pkg.NewWriter("/tmp/data")
+	writer, err := pkg.NewWriter(fsPath)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	//data := []byte("HellHello worlddddffff")
-	size := uint(4)
-
-	data, err := manager.ReadFile("/home/user/test/cosi.mp4")
+	data, err := manager.ReadFile(path)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	dataFile := pkg.CreateDataFile(data, size, "SHA1")
+	dataFile := pkg.CreateDataFile(data, blockSize, hash)
 
-	err = writer.Write(dataFile, "test")
-	log.Print(err)
-
-	result, err := writer.Read(dataFile, "test")
-	log.Print(err)
-
-	//log.Printf("in: %s\n", string(data))
-	//log.Printf("out: %s\n", string(result))
-	log.Printf("%d = %d", len(data), len(result))
-
-	err = manager.WriteFile("/home/user/test/cosi2.mp4", result)
+	return writer.Write(dataFile, name)
 	if err != nil {
-		log.Fatal(err)
+		return err
+	}
+
+	return nil
+}
+
+func performRead(name, path string) error {
+	manager := &pkg.FSManager{}
+	writer, err := pkg.NewWriter(fsPath)
+	if err != nil {
+		return err
+	}
+
+	//get DataFile FromDB
+	dataFile := &pkg.DataFile{}
+
+	result, err := writer.Read(dataFile, name)
+	if err != nil {
+		return err
+	}
+
+	err = manager.WriteFile(path, result)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func performDelete(name string) error {
+	// delete file from DB
+	return nil
+}
+
+func performList() error {
+	// print file list from DB
+	return nil
+}
+
+func main() {
+	var (
+		err error
+	)
+
+	flag.Parse()
+
+	switch {
+	case *modeW:
+		err = performWrite(*name, *path, *hash, *size)
+	case *modeR:
+		err = performRead(*name, *path)
+	case *modeD:
+		err = performDelete(*name)
+	case *modeL:
+		err = performList()
+	}
+
+	if err != nil {
+		log.Fatalf("err: %+v", err)
 	}
 }
